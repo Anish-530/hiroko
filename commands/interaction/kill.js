8ee9f460c9c82ef721c644c6673bff2e6f400d39
Lynx530 <67557159+Lynx530@users.noreply.github.com>
2020-08-01 20:21:39 +0530

    Update kill.js

--- commands/interaction/kill.js
+++ commands/interaction/kill.js
@@ -14,26 +14,27 @@ module.exports={
     aliases: [],
     usage: 'hey kill <member>',
     run: async(bot, message, args)=>{
-        const mentionedddMember111 = message.mentions.users.first()
-        if (mentionedddMember111.id === message.guild.me.id) return message.channel.send(`*Dies x_x*`)
-        if (mentionedddMember111.id === message.author.id) {
-            Tenor.Search.Random("anime wasted", "1").then(Results111 => {
-                Results111.forEach(Post111 => {
-                    var mediax111 = Post111.media[0];
-                    console.log(mediax111.gif);
-                    var gif111 = mediax111.gif
-                    console.log(gif111)
-                    let ki11 = new Discord.MessageEmbed()
+        const mentionedddMember1112 = message.mentions.users.first()
+        if (mentionedddMember1112.id === message.guild.me.id) return message.channel.send(`*Dies x_x*`)
+        if (mentionedddMember1112.id === message.author.id) {
+            Tenor.Search.Random("anime wasted", "1").then(Results1112 => {
+                Results1112.forEach(Post111 => {
+                    var mediax1112 = Post1112.media[0];
+                    console.log(mediax1112.gif);
+                    var gif1112 = mediax1112.gif
+                    console.log(gif1112)
+                    let ki112 = new Discord.MessageEmbed()
                     
-                    ki11.setColor(0x2f3136)
-                    ki11.setDescription(`**${message.author.username}** killed themselves 🤕`)
-                    ki11.setImage(gif111.url)
-                    ki11.setTimestamp(new Date())
-                    ki11.setFooter("Hiroko", bot.user.avatarURL())
-                    return slapp.edit("\t", ki11)
+                    ki112.setColor(0x2f3136)
+                    ki112.setDescription(`**${message.author.username}** killed themselves 🤕`)
+                    ki112.setImage(gif111.url)
+                    ki112.setTimestamp(new Date())
+                    ki112.setFooter("Hiroko", bot.user.avatarURL())
+                    return slapp.edit("\t", ki112)
                 });
             }).catch(console.error); 
         }
+        else if(mentionedddMember1112) {
         const slapp = await message.channel.send("**Killing...**")
         Tenor.Search.Random("anime wasted", "1").then(Results111 => {
             Results111.forEach(Post111 => {
@@ -51,5 +52,6 @@ module.exports={
                 return slapp.edit("\t", ki11)
             });
         }).catch(console.error); 
+            }
         }
     }
