ad518dff2d9750603a81b334276e02c2c1e5df9e
Lynx530 <67557159+Lynx530@users.noreply.github.com>
2020-08-01 19:27:01 +0530

    Update lick.js

--- commands/interaction/lick.js
+++ commands/interaction/lick.js
@@ -14,10 +14,10 @@ module.exports={
     aliases: [],
     usage: 'hey lick <member>',
     run: async(bot, message, args)=>{
-        if (!args[0]) { return message.channel.send("*You can lick me UwU*") }
-        const mentionedddMember111 = message.mentions.users.first()
+        if (!args[0]) return message.channel.send("*You can lick me UwU*")
+        const mentionedddMember111 = message.mentions.users.first() || message.author;
         if (mentionedddMember111.id === message.guild.me.id) { return message.channel.send(`*>w<*`) }
-        else if (mentionedddMember111.id === message.author) { return message.channel.send("Do you taste good?") }
+        else if (mentionedddMember111.id === message.author.id) { return message.channel.send("Do you taste good?") }
         const slapp = await message.channel.send("**Licking...**")
         Tenor.Search.Random("anime lick", "1").then(Results111 => {
             Results111.forEach(Post111 => {
