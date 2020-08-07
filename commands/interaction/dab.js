5e1f959f8e4ec9a7158ab90055309175cc2a5240
Lynx530 <67557159+Lynx530@users.noreply.github.com>
2020-08-07 16:29:55 +0530

    adding of dab.js
    dab.js was successfully added, yet to test

---
+++ commands/interaction/dab.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js');
+const Tenor = require("tenorjs").client({
+    "Key": "JLH9C6HHQDVR", // https://tenor.com/developer/keyregistration
+    "Filter": "off", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+    "MediaFilter": "basic", // either minimal or basic, not case sensitive
+    "DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
+});
+const {MessageEmbed} = require('discord.js');
+module.exports={
+    name: 'dab',
+    category: 'interaction',
+    description: 'Dab dab muscle up!',
+    aliases: [],
+    usage: 'hey dab [user]',
+    run: async(bot, message, args)=>{
+        const mentionedddMembere222 = message.mentions.users.first()
+        if (message.mentions.members.first() !== undefined) {
+            if (message.mentions.members.first().id === bot.user.id) return message.channel.send("*Dabs back!*") 
+        }
+        const screammm = await message.channel.send("\`Dabbing....\`")
+        let sc = new Discord.MessageEmbed()
+        if (mentionedddMembere222 === message.author) {
+            Tenor.Search.Random("anime dab", "1").then(Resultsr => {
+                Resultsr.forEach(Postr => {
+                    var mediaxr = Postr.media[0];
+                    console.log(mediaxr.gif);
+                    var gifr = mediaxr.gif
+                    console.log(gifr)
+                    
+                    sc.setColor(0x2f3136)
+                    sc.setDescription(`**${message.author.username}** dabs 😼`)
+                    sc.setImage(gifr.url)
+                    sc.setTimestamp(new Date())
+                    sc.setFooter("Hiroko", bot.user.avatarURL())
+                    return screammm.edit("\t", sc)
+                })
+            })
+        }
+        if (mentionedddMembere222) {
+            Tenor.Search.Random("anime dab", "1").then(Resultsr => {
+                Resultsr.forEach(Postr => {
+                    var mediaxr = Postr.media[0];
+                    var gifr = mediaxr.gif
+            
+                    sc.setColor(0x2f3136)
+                    sc.setDescription(`**${message.author.username}** dabs at **${mentionedddMembere222.username}** 😼`)
+                    sc.setImage(gifr.url)
+                    sc.setTimestamp(new Date())
+                    sc.setFooter("Hiroko", bot.user.avatarURL())
+                    return screammm.edit("\t", sc)
+                });
+            })
+        }
+        else if (!mentionedddMembere222) {
+            Tenor.Search.Random("anime dab", "1").then(Resultsr => {
+                Resultsr.forEach(Postr => {
+                    var mediaxr = Postr.media[0];
+
+                    var gifr = mediaxr.gif
+              
+                
+                    sc.setColor(0x2f3136)
+                    sc.setDescription(`**${message.author.username}** dabs 😼`)
+                    sc.setImage(gifr.url)
+                    sc.setTimestamp(new Date())
+                    sc.setFooter("Hiroko", bot.user.avatarURL())
+                    return screammm.edit("\t", sc)
+                });
+            }).catch(console.error);
+        }
+    }
+}
