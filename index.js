ea62d5ba2b06fdcea9aac8929ce98a6d5d92609c
Lynx530 <67557159+Lynx530@users.noreply.github.com>
2020-08-11 08:04:38 +0530

    just a change to the status

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const PREFIX = 'hey ';
 
 
 bot.on('ready', () => {
-    bot.user.setActivity(`hey help 🟣 on ${bot.guilds.cache.size} guilds 🟣 from ${bot.users.cache.size} users`, {type: "PLAYING"});
+    bot.user.setActivity(`hey help 🟣 on ${bot.guilds.cache.size} guilds 🟣 from ${bot.users.cache.size} users`, {type: "LISTENING"});
     console.log(`Hello, I am online on ${bot.guilds.cache.size} servers and serving ${bot.users.cache.size} users`);
 })
 
