6374cabe0f6285596bdebc4f1df206e97e71e8de
Awish-Senpai <69074469+Awish-Senpai@users.noreply.github.com>
2020-10-05 11:28:13 +0800

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -7,9 +7,9 @@ module.exports={
     aliases: [],
     usage: 'hey kick <mention the user you want to kick>',
     run: async(bot, message, args)=>{
-        if (!message.member.hasPermission('KICK_MEMBERS')) return message.reply('You don\'t have the permission to use this command.\nYou need \`KICK_MEMBERS\` permission, to use this command.');
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send('You don\'t have the permission to use this command.\nYou need \`KICK_MEMBERS\` permission, to use this command.');
         if(!message.member.guild.me.hasPermission(['KICK_MEMBERS'])) return message.channel.send("I don\'t have the permission to \`KICK_MEMBERS\`.\nPlease provide me the following permission to use this command")
-        if (!args[0]) return message.reply('You need to Mention whom you want to kick :) . Ex: hey kick @someone').then(message => message.delete({ timeout: 3000 }));
+        if (!args[0]) return message.reply('You need to Mention whom you want to kick :) . Ex: \`hey kick @someone\` or \`hey kick User-ID\`').then(message => message.delete({ timeout: 10000 }));
 
         const userf = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
         if(userf.id === message.author.id) return message.reply('Uh! You can\'t kick yourself, you know? :/', {
