5e4c0528ff7608af9cfd3065a98f0e44a42f533c
Awish-Senpai <69074469+Awish-Senpai@users.noreply.github.com>
2020-10-05 11:21:00 +0800

    Update createchannel.js

--- commands/moderation/createchannel.js
+++ commands/moderation/createchannel.js
@@ -7,8 +7,8 @@ module.exports={
     aliases: [],
     usage: 'hey createchannel <channel name>',
     run: async(bot, message, args)=>{
-        if(!message.member.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send("Sorry, your roles are too low to execute this command!")
-        if(!message.member.guild.me.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send("I don\'t have the permission to \`MANAGE CHANNELS\`.\nPlease provide me the following permission to use this command")
+        if(!message.member.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send("You don\'t have the permission to use this command.\nYou need \`MANAGE_CHANNELS\` permission, to use this command.")
+        if(!message.member.guild.me.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send("I don\'t have the permission to \`MANAGE_CHANNELS\`.\nPlease provide me the following permission to use this command")
         const vb = message.content.toLowerCase().substring(18);
         let dedle = args.join('-')
         if(!vb) return message.channel.send("Provide me a channel name to create it!\n`Example : hey createchannel waifu warzone`")
