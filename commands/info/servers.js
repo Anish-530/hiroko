986686d31c771d991dc8f5d788744f39d033e259
Awish-Senpai <69074469+Awish-Senpai@users.noreply.github.com>
2020-10-07 03:09:53 +0800

    Update servers.js

--- commands/info/servers.js
+++ commands/info/servers.js
@@ -10,7 +10,8 @@ module.exports={
 	if(message.author.id !== "671355502399193128") return;
         try{
             bot.guilds.cache.forEach(guild => {
-                message.channel.send(guild.name)
+                var memberCount = guild.members.cache.filter(member => !member.user.bot).size;
+                message.channel.send(`**${guild.name}** has \`${memberCount}\` members`)
                 })
         }catch(err){
             message.channel.send(err.message);
